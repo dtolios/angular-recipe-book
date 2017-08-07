33e08cddac5073a87516e2ffe8ac88548ce16833
Dimitri Tolios <dtolios@riotgames.com>
2017-08-07 09:02:09 -0700

    Added data binding and repeat directives to the recipe detail page

--- public/scripts/controllers/RecipeDetailController.js
+++ public/scripts/controllers/RecipeDetailController.js
@@ -2,6 +2,7 @@
 (function () {
     'use strict';
     angular.module('app').controller('RecipeDetailController', function ($scope, $location, dataService) {
+        // conditional logic for whether user is creating a new recipe or editing an existing recipe
         if($location.path() === '/add') {
             $scope.save = function () {
                 dataService.createRecipe(function () {
